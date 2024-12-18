f25fa777068369d54e3c97c53f9763bf4bc77a00
Jose2480 <joseserpamedinaxd@gmail.com>
2024-12-18 17:35:36 -0500

    Enunciado arbol de navidad + Ejercicio resuelto con nuevas instrucciones

--- JAVA/Ejercicios/TodoCode/ArbolNavidad/src/arbolnavidad/ArbolNavidad.java
+++ JAVA/Ejercicios/TodoCode/ArbolNavidad/src/arbolnavidad/ArbolNavidad.java
@@ -1,51 +1,58 @@
 package arbolnavidad;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class ArbolNavidad {
 
     public static void main(String[] args) {
 
-        int tamArbol;
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in); //Obtener datos por consola
+        int tamArbol; //Inicializar variable del tamaño del arbol
+        String CaracteresDeseados = "#+ox*"; //Caracteres que quiero que se generen
+        Random rnd = new Random(); // Variable para generar el valor aleatorio
 
+        //Establecer el tamaño del arbol (Debe ser mayor o igual a 3)
         do {
-            System.out.print("De que tamaño quieres que sea el arbol? (Min. altura 3): ");
+            System.out.print("¿De qué tamaño desea que sea el arbol? (Altura min: 3): ");
             tamArbol = entrada.nextInt();
         } while (tamArbol < 3);
 
-        //Bucle para cada fila del arbol
-        for (int fila = 0; fila <= tamArbol - 1; fila++) {
+        //Dibujar cada fila del arbol
+        for (int fila = 0; fila < tamArbol; fila++) {
 
-            //bucle para los espacios
+            //Dibujar espacios de cada fila
             for (int espacios = 0; espacios < (tamArbol - fila - 1); espacios++) {
                 System.out.print(" ");
             }
 
-            //bucle para los asteriscos
-            for (int asterisco = 0; asterisco < (fila * 2) + 1; asterisco++) {
-                System.out.print("*");
-            }
+            //Dibujar Elementos del arbol (Hojas y Decoraciones)
+            for (int elementos = 0; elementos <= (fila * 2); elementos++) {
+                if (elementos == 0 || elementos == (fila * 2)) { //Si es el primer elemento del arbol o el último, se pinta como un *
+                    System.out.print("*");
+                } else { //El resto de elementos se pintan de forma aleatoria entre: #, +, o, x, *
+                    char decoracion;
+                    //Si el elemento no se encuentra entre los deseados, se vuelve a generar otro de forma aleatoria
+                    do {
+                        decoracion = (char) (rnd.nextInt(121) + 35);
+                    } while (CaracteresDeseados.indexOf(decoracion) < 0);
 
+                    System.out.print(decoracion);
+                }
+            }
             System.out.println("");
         }
 
-        //Crear tronco
-        int largoTronco = tamArbol - 2;
-        for (int base = 0; base < largoTronco; base++) {
-            
-            //Espacios
-            for (int espacios = 0; espacios < (tamArbol - 2); espacios++) {
-                System.out.print(" ");
-            }
-            
-            //Barritas
-            for (int barritas = 0; barritas < 3; barritas++) {
-                System.out.print("|");
-            }
-            
-            System.out.println("");
+        //Dibujar espacios del tronco
+        for (int espaciosTronco = 0; espaciosTronco < (tamArbol - 2); espaciosTronco++) {
+            System.out.print(" ");
         }
+
+        //Dibujar Tronco
+        for (int Tronco = 0; Tronco < 3; Tronco++) {
+            System.out.print("|");
+        }
+        
+        System.out.println("");
     }
-
 }
