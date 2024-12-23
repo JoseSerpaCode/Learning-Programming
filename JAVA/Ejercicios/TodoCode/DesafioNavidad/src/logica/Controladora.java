4ebaf20fbc19125d5f5340d8b318c04e7f6da09a
JoseSerpaCode <joseserpamedinaxd@gmail.com>
2024-12-22 21:03:01 -0500

    EJERCICIO FALLIDO!! T_T / Juego Adivinanzas

---
+++ JAVA/Ejercicios/TodoCode/DesafioNavidad/src/logica/Controladora.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class Controladora {
+    
+    public int determinarCantidadEmojis(String emoji, int cantidad, String[][] matriz){
+        
+        return 0;
+    }
+    
+    public String[][] generarMatriz(){
+        
+        String[][] matriz = new String [1][1];
+        
+        return matriz;
+    }
+}
---
+++ JAVA/Ejercicios/TodoCode/DesafioNavidad/src/logica/main.java
@@ -0,0 +1,11 @@
+package logica;
+
+import gui.Interfaz;
+
+public class main {
+    public static void main(String[] args) {
+        Interfaz mensajero = new Interfaz();
+        mensajero.setVisible(true);
+        mensajero.setLocationRelativeTo(null);
+    }
+}
