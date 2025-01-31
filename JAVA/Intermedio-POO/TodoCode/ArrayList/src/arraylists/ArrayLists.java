9b218c56f1a4dbc7b555dde480f4c60ec27a108b
JoseSerpaCode <joseserpamedinaxd@gmail.com>
2025-01-31 11:32:31 -0500

    Se realiza la prueba de ArrayLists y se modifica el nombre del paquete

---
+++ JAVA/Intermedio-POO/TodoCode/ArrayList/src/arraylists/ArrayLists.java
@@ -0,0 +1,18 @@
+package arraylists;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ArrayLists {
+
+    public static void main(String[] args) {
+        List<Persona> lista = new ArrayList<Persona> ();
+        lista.add(new Persona(1, "Juan", 18));
+        lista.add(new Persona(2, "Andres", 20));
+
+        for (Persona perso : lista){
+            System.out.println(perso.getNombre());
+        }
+    }
+}
--- JAVA/Intermedio-POO/TodoCode/ArrayList/src/arraylists/Persona.java
+++ JAVA/Intermedio-POO/TodoCode/ArrayList/src/arraylists/Persona.java
@@ -1,4 +1,4 @@
-package arraylist;
+package arraylists;
 
 public class Persona {
     private int num;
