3ba0cfaccddc6f6cd511d6cfa2564ef2d726cf9c
JoseSerpaCode <joseserpamedinaxd@gmail.com>
2025-04-09 19:18:44 -0500

    Refactor EjercicioUno.cpp to use vector for dynamic matrix allocation; improve matrix filling and displaying functions

--- CPP/Matrices/EjercicioUno.cpp
+++ CPP/Matrices/EjercicioUno.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+void llenarMatriz(vector<vector<int>> matriz, int filas, int columnas){
+	srand(time(NULL));
+	
+	for(int i = 0; i < filas; i++) {
+		for(int j = 0; j < columnas; j++) {
+			matriz[i][j] = rand () % 6 + 0;
+		}
+	}
+}
+
+void mostrarMatriz(vector<vector<int>> matriz, int filas, int columnas){
+	for(int i = 0; i < filas; i++) {
+		for(int j = 0; j < columnas; j++) {
+			cout << matriz[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
 int main() {
-    int n, m;
-    cout << "Ingrese el número de filas: ";
-    cin >> n;
-    cout << "Ingrese el número de columnas: ";
-    cin >> m;
-
-    int matriz[n][m];
-
-    // Llenar la matriz con números aleatorios
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            matriz[i][j] = rand() % 5;
-        }
-    }
-
-    // Mostrar la matriz
-    cout << "La matriz es:" << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << matriz[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    return 0;
+	
+	int cantEstudiantes, cantMaterias;
+	cout << "Ingrese la cantidad de estudiantes: ";
+	cin >> cantEstudiantes;
+	cout << "Ingrese la cantidad de materias: ";
+	cin >> cantMaterias;
+	
+	vector<vector<int>> matriz(cantEstudiantes, vector<int>(cantMaterias));
+	llenarMatriz(matriz, cantEstudiantes, cantMaterias);
+	mostrarMatriz(matriz, cantEstudiantes, cantMaterias);
+	
+	return 0;
 }
